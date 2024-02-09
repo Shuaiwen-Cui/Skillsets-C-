22e7eb8619f69c08fab9dfdd67ea9c28e68a6227
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2024-02-09 17:08:37 +0800

    auto commit by win-upload.bat

---
+++ C++PrimerPlus/CH03/3.2/exceed.cpp
@@ -0,0 +1,28 @@
+// exceed.cpp -- exceeding some interger limits
+#include <iostream>
+#define ZERO 0 // make ZERO symble for 0 value
+#include <climits> // defines INT_MAX as largest int value
+int main()
+{
+    using namespace std;
+    short sam = SHRT_MAX; // initialize a variable to max value
+    unsigned short sue = sam; // okay if variable sam already defined
+
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Add $1 to each account." << endl << "Now ";
+    sam = sam + 1;
+    sue = sue + 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nPoor Sam!" << endl;
+    sam = ZERO;
+    sue = ZERO;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Take $1 from each account." << endl << "Now ";
+    sam = sam - 1;
+    sue = sue - 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl << "Lucky Sue!" << endl;
+    return 0;
+}
